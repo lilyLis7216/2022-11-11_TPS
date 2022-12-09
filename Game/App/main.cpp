42cb91b5e0f7c022fa5b33a4a84a986fd9b6cb2e
fca406-09 <>
2022-12-09 12:20:14 +0900

    GameObjectManagerの呼び出し

--- Game/App/main.cpp
+++ Game/App/main.cpp
@@ -1,8 +1,9 @@
 #pragma warning(disable:28251)
 #include <windows.h>
 #include "Dxlib.h"
-#include "Manager/GameManager.h"
 #include "Manager/AssetManager.h"
+#include "Manager/GameManager.h"
+#include "Manager/GameObjectManager.h"
 #include "Scene/Title.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -13,11 +14,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     /** ゲームマネージャの生成*/
     My3dApp::GameManager::CreateInstance();
 
+    /** ゲームオブジェクトマネージャの生成*/
+    My3dApp::GameObjectManager::CreateInstance();
+
     /** 最初のシーンをセット*/
     My3dApp::GameManager::SetFirstScene(new My3dApp::Title());
 
     /** ゲームループ*/
     My3dApp::GameManager::Loop();
+
+    /** ゲームオブジェクトマネージャの削除*/
+    My3dApp::GameObjectManager::DeleteInstance();
 
     /** ゲームマネージャの削除*/
     My3dApp::GameManager::DeleteInstance();
