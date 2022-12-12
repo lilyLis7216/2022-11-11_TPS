d6a684b438240c6282ebc252528326b1e4ae5279
fca408-27 <>
2022-12-12 13:52:57 +0900

    GamePadの更新処理、取得処理の修正

--- Game/App/Library/GamePad.cpp
+++ Game/App/Library/GamePad.cpp
@@ -3,8 +3,30 @@
 namespace My3dApp
 {
     GamePad* GamePad::instance = nullptr;
+
     XINPUT_STATE GamePad::padState;
-    bool GamePad::padInput[16] = { 0 };
+
+    int GamePad::buttonInput = 0x00;
+
+    const unsigned int BIT_FLAG[] =
+    {
+        (1 << 0),   // デジタル方向ボタン上
+        (1 << 1),   // デジタル方向ボタン下
+        (1 << 2),   // デジタル方向ボタン左
+        (1 << 3),   // デジタル方向ボタン右
+        (1 << 4),   // STARTボタン
+        (1 << 5),   // BACKボタン
+        (1 << 6),   // 左スティック押し込み
+        (1 << 7),   // 右スティック押し込み
+        (1 << 8),   // LBボタン
+        (1 << 9),   // RBボタン
+        (1 << 10),  // 
+        (1 << 11),  // 
+        (1 << 12),  // Aボタン
+        (1 << 13),  // Bボタン
+        (1 << 14),  // Xボタン
+        (1 << 15),  // Yボタン
+    };
 
     GamePad::GamePad()
     {
@@ -40,17 +62,23 @@ namespace My3dApp
         /** 全ボタンの入力確認*/
         for (int i = 0; i < 16; i++)
         {
-            if (i < 6 || 11 < i)
+            if (padState.Buttons[i])
             {
-                if (padState.Buttons[i])
-                {
-                    padInput[i] = true;
-                }
-                else
-                {
-                    padInput[i] = false;
-                }
+                buttonInput |= (1 << BIT_FLAG[i]);
+            }
+            else
+            {
+                buttonInput &= ~(1 << BIT_FLAG[i]);
             }
         }
+    }
+
+    bool GamePad::GetInput(const int buttonNumber)
+    {
+        if (buttonInput & (1 << BIT_FLAG[buttonNumber]))
+        {
+            return true;
+        }
+        return false;
     }
 }/** namespace My3dApp*/
--- Game/App/Library/GamePad.h
+++ Game/App/Library/GamePad.h
@@ -2,39 +2,115 @@
 
 #include "DxLib.h"
 
+using namespace std;
+
 namespace My3dApp
 {
+    /// <summary>
+    /// ゲームパッドクラス
+    /// </summary>
     class GamePad final
     {
     private:
-        /** コンストラクタ*/
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         GamePad();
 
-        /** デストラクタ*/
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
         ~GamePad();
 
-        /** ゲームパッドのインスタンス*/
+        // ゲームパッドクラスの唯一のインスタンス
         static GamePad* instance;
 
-        /** パッドの入力状態*/
+        // パッドの入力状態
         static XINPUT_STATE padState;
 
-        static bool padInput[16];
+        // ボタンの入力識別用
+        static int buttonInput;
 
     public:
+        /// <summary>
+        /// インスタンスの生成
+        /// </summary>
         static void CreateInstance();
+
+        /// <summary>
+        /// インスタンスの削除
+        /// </summary>
         static void DeleteInstance();
+
+        /// <summary>
+        /// ゲームパッドの更新
+        /// </summary>
         static void Update();
 
-        static bool GetUp() { return padInput[0]; }
-        static bool GetDown() { return padInput[1]; }
-        static bool GetLeft() { return padInput[2]; }
-        static bool GetRight() { return padInput[3]; }
-        static bool GetStart() { return padInput[4]; }
-        static bool GetBack() { return padInput[5]; }
-        static bool GetButtonA() { return padInput[12]; }
-        static bool GetButtonB() { return padInput[13]; }
-        static bool GetButtonX() { return padInput[14]; }
-        static bool GetButtonY() { return padInput[15]; }
+        // ボタンの列挙型（マジックナンバー回避用）
+        enum ButtonNum
+        {
+            UP = 0,  // デジタル方向ボタン上
+            DOWN,    // デジタル方向ボタン下
+            LEFT,    // デジタル方向ボタン左
+            RIGHT,   // デジタル方向ボタン右
+            START,   // STARTボタン
+            BACK,    // BACKボタン
+            LSTICK,  // 左スティック押し込み
+            RSTICK,  // 右スティック押し込み
+            LB,      // LBボタン
+            RB,      // RBボタン
+            NONE1,   // 割り振りなし
+            NONE2,   // 割り振りなし
+            A,       // Aボタン
+            B,       // Bボタン
+            X,       // Xボタン
+            Y,       // Yボタン
+        };
+
+        /// <summary>
+        /// ボタンの入力状態の取得
+        /// </summary>
+        /// <param name="buttonNumber">取得したいボタンの番号</param>
+        /// <returns>入力されていたらtrue そうでなければfalse</returns>
+        static bool GetInput(const int buttonNumber);
+
+        /// <summary>
+        /// 左トリガーの取得
+        /// </summary>
+        /// <returns></returns>
+        static const unsigned char GetLeftTrigger() { return padState.LeftTrigger; }
+
+        /// <summary>
+        /// 右トリガーの取得
+        /// </summary>
+        /// <returns></returns>
+        static const unsigned char GetRightTrigger() { return padState.RightTrigger; }
+
+        /// <summary>
+        /// 左スティックのX座標
+        /// </summary>
+        /// <returns></returns>
+        static const short GetLeftStickX() { return padState.ThumbLX; }
+
+        /// <summary>
+        /// 左スティックのY座標
+        /// </summary>
+        /// <returns></returns>
+        static const short GetLeftStickY() { return padState.ThumbLY; }
+
+        /// <summary>
+        /// 右スティックのX座標
+        /// </summary>
+        /// <returns></returns>
+        static const short GetRightStickX() { return padState.ThumbRX; }
+
+        /// <summary>
+        /// 右スティックのY座標
+        /// </summary>
+        /// <returns></returns>
+        static const short GetRightStickY() { return padState.ThumbRY; }
     };
+
+#define Button GamePad::ButtonNum
 }/** namespace My3dApp*/
