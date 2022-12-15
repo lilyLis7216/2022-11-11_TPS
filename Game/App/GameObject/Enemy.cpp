841e9447dd6936f1498bb7d965d4a51c5c7d38fd
fca408-27 <>
2022-12-15 19:39:49 +0900

    Enemyの作成

---
+++ Game/App/GameObject/Enemy.cpp
@@ -0,0 +1,254 @@
+#include "Enemy.h"
+#include "../Manager/AssetManager.h"
+#include "../Library/AnimationController.h"
+#include "../Library/Calc3D.h"
+#include "../Library/GamePad.h"
+
+namespace My3dApp
+{
+    Enemy::Enemy()
+        : Enemy(initPos)
+    {
+    }
+
+    Enemy::Enemy(VECTOR pos)
+        : GameObject(ObjectTag::Enemy, pos)
+        , animCtrl(nullptr)
+        , animTypeID(0)
+        , isRotate(false)
+        , moveCount(10.0f)
+        , isMove(false)
+        , moveTmp(0)
+    {
+        modelHandle = AssetManager::GetMesh("../Assets/Model/Enemy/unityChanModel.mv1");
+
+        MV1SetScale(modelHandle, VGet(0.01f, 0.01f, 0.01f));
+
+        dir = VGet(1, 0, 0);
+
+        aimDir = dir;
+
+        speed = VGet(0, 0, 0);
+
+        collisionType = CollisionType::Sphere;
+
+        collisionSphere.localCenter = VGet(0, 50.0f, 0);
+
+        collisionSphere.radius = 30.0f;
+
+        collisionLine = LineSegment(VGet(0.0f, 20.0f, 0.0f), VGet(0.0f, -30.0f, 0.0f));
+    }
+
+    Enemy::~Enemy()
+    {
+        // モデルのアンロード
+        AssetManager::ReleaseMesh(modelHandle);
+
+        // アニメーションコントローラの削除
+        delete animCtrl;
+    }
+
+    void Enemy::Update(float deltaTime)
+    {
+        Move(deltaTime);
+
+        RotateCheck();
+    }
+
+    void Enemy::Draw()
+    {
+        // モデルの描画
+        MV1DrawModel(modelHandle);
+
+        DrawCollider();
+    }
+
+    void Enemy::OnCollisionEnter(const GameObject* other)
+    {
+        // 調べるオブジェクトのタグを取得
+        ObjectTag tag = other->GetTag();
+
+        // マップとの衝突
+        if (tag == ObjectTag::Map)
+        {
+            int collModel = other->GetCollisionModel();
+
+            // マップと自身の境界旧都の当たり判定
+            MV1_COLL_RESULT_POLY_DIM collInfo;
+
+            // 当たっている場合
+            if (CollisionPair(collisionSphere, collModel, collInfo))
+            {
+                // 押し戻し量
+                VECTOR pushBackVec = CalcSpherePushBackVecFromMesh(collisionSphere, collInfo);
+
+                // 押し戻し
+                pos += pushBackVec;
+
+                // 当たり判定情報の解放
+                MV1CollResultPolyDimTerminate(collInfo);
+
+                // 当たり判定の更新
+                CollisionUpdate();
+            }
+
+            // 背景と足元線分当たり判定
+            MV1_COLL_RESULT_POLY collInfoLine;
+
+            // 当たっている場合
+            if (CollisionPair(collisionLine, collModel, collInfoLine))
+            {
+                // 足元を衝突点に合わせる
+                pos = collInfoLine.HitPosition;
+
+                // 当たり判定の更新
+                CollisionUpdate();
+            }
+        }
+    }
+
+    void Enemy::Move(float deltaTime)
+    {
+        if (moveCount > 0)
+        {
+            moveCount -= deltaTime;
+        }
+        else
+        {
+            moveCount = 3.0f;
+            moveTmp = rand() % 4;
+        }
+
+        // 上方向
+        VECTOR UP = { 0, 0, 1 };
+
+        // 下方向
+        VECTOR DOWN = { 0, 0, -1 };
+
+        // 左方向
+        VECTOR LEFT = { -1, 0, 0 };
+
+        // 右方向
+        VECTOR RIGHT = { 1, 0, 0 };
+
+        // 移動用ベクトル
+        VECTOR inputVec = VGet(0, 0, 0);
+
+        // 入力があったかどうか
+        bool input = false;
+
+        switch (moveTmp)
+        {
+        case 0:
+            inputVec += UP;
+            input = true;
+            break;
+        case 1:
+            inputVec += DOWN;
+            input = true;
+            break;
+        case 2:
+            inputVec += LEFT;
+            input = true;
+            break;
+        case 3:
+            inputVec += RIGHT;
+            input = true;
+            break;
+        default:
+            break;
+        }
+
+        // 入力があったら
+        if (input)
+        {
+            // 左右上下同時押しなどで入力ベクトルが0のとき
+            if (VSquareSize(inputVec) < 0.5f)
+            {
+                return;
+            }
+
+            // 方向を正規化
+            inputVec = VNorm(inputVec);
+
+            // 入力方向は現在向いている方向と異なるか
+            if (IsNearAngle(inputVec, dir))
+            {
+                dir = inputVec;
+            }
+            else
+            {
+                isRotate = true;
+                aimDir = inputVec;
+            }
+
+            speed = inputVec + (inputVec * deltaTime * 200.0f);
+
+            // もし他のモーション中だったら走りモーションへ
+            if (animTypeID != 1)
+            {
+                animTypeID = 1;
+                //animCtrl->StartAnimation(animTypeID);
+            }
+        }
+        else
+        {
+            speed *= 0.9f;
+
+            // もしほかのモーション中だったら歩きモーションへ
+            if (animTypeID != 0)
+            {
+                animTypeID = 0;
+                //animCtrl->StartAnimation(animTypeID);
+            }
+        }
+
+        pos += speed;
+
+        // 3Dモデルのポジション設定
+        MV1SetPosition(modelHandle, pos);
+
+        // 向きに合わせてモデルを回転
+        MATRIX rotYMat = MGetRotY(180.0f * (float)(DX_PI / 180.0f));
+
+        VECTOR negativeVec = VTransform(dir, rotYMat);
+
+        // モデルに回転をセットする
+        MV1SetRotationZYAxis(modelHandle, negativeVec, VGet(0.0f, 1.0f, 0.0f), 0.0f);
+
+        // 当たり判定モデルの位置更新
+        CollisionUpdate();
+    }
+
+    void Enemy::RotateCheck()
+    {
+        if (isRotate)
+        {
+            // 回転が目標角度に十分近ければ回転終了
+            if (IsNearAngle(aimDir, dir))
+            {
+                dir = aimDir;
+                isRotate = false;
+            }
+            else
+            {
+                // 回転させる
+                VECTOR interPolateDir = RotateForAimVecYAxis(dir, aimDir, 10.0f);
+
+                // 回転が目標角度を超えていないか
+                VECTOR cross1 = VCross(dir, aimDir);
+                VECTOR cross2 = VCross(interPolateDir, aimDir);
+
+                // 目標角度を超えたら終了
+                if (cross1.y * cross2.y < 0.0f)
+                {
+                    interPolateDir = aimDir;
+                    isRotate = false;
+                }
+
+                // 目標ベクトルに10度だけ近づけた角度
+                dir = interPolateDir;
+            }
+        }
+    }
+}// namespace My3dApp
---
+++ Game/App/GameObject/Enemy.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "GameObject.h"
+
+namespace My3dApp
+{
+    /// <summary>
+    /// エネミー（派生クラス、継承元：ゲームオブジェクト）
+    /// </summary>
+    class Enemy : public GameObject
+    {
+    private:
+        // 向き
+        VECTOR dir;
+
+        // 目標向き
+        VECTOR aimDir;
+
+        // 速度
+        VECTOR speed;
+
+        // アニメーション管理用
+        class AnimationController* animCtrl;
+
+        // アニメーション種類のID
+        int animTypeID;
+
+        // 回転中か
+        bool isRotate;
+
+        float moveCount;
+
+        float isMove;
+
+        int moveTmp;
+
+        void Move(float deltaTime);
+
+        void RotateCheck();
+
+    public:
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        Enemy();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="pos">初期位置</param>
+        Enemy(VECTOR pos);
+
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~Enemy();
+
+        /// <summary>
+        /// エネミーの更新
+        /// </summary>
+        /// <param name="deltaTime">1フレームの経過時間</param>
+        void Update(float deltaTime) override;
+
+        /// <summary>
+        /// エネミーの描画
+        /// </summary>
+        void Draw() override;
+
+        /// <summary>
+        /// 他のオブジェクトと衝突したときのリアクション
+        /// </summary>
+        /// <param name="other">自分とは異なるオブジェクト</param>
+        void OnCollisionEnter(const GameObject* other) override;
+    };
+}// namespace My3dApp
